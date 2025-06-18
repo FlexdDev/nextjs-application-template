5422313a8512ee706810e43126221bda8ec6c702
Flexd <70884953+FlexdDev@users.noreply.github.com>
2025-06-18 23:52:12 +0300

    Add next.config.js 1750279931563

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
   },
+  basePath: '',
+  assetPrefix: '/',
 }
 
 module.exports = nextConfig
