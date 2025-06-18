667923b00e2cb3aa2e7fa8676343b11bb6c8c705
Flexd <70884953+FlexdDev@users.noreply.github.com>
2025-06-18 23:39:19 +0300

    Add .next/static/chunks/webpack.js 1750279158666

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a01a82fedb16879"; }
+/******/ 		__webpack_require__.h = function() { return "dce3b0da86af506f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
